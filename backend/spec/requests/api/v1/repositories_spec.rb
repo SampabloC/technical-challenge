9614c8c1f4682a79d22102c39ebb29f569c40e3e
SampabloC <sampablo-jisc@outlook.com>
2022-02-13 01:34:38 -0600

    Add unit test for Repositories Test list and search methods

--- backend/spec/requests/api/v1/repositories_spec.rb
+++ backend/spec/requests/api/v1/repositories_spec.rb
@@ -3,12 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Repositories', type: :request do
-  describe 'GET /index' do
-    it 'gets repositories for yknx4' do
-      get api_v1_user_repositories_path(user_id: 'yknx4')
+  describe 'Check methods' do
+    it 'gets repositories list successfully' do
+      get '/api/v1/repos/list'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'search for a repository' do
+      get api_v1_users_path, params: { username: 'SampabloC' }
+      get '/api/v1/repos/search', params: { repository_name: "technical-challenge" }
       body = JSON.parse(response.body)
       expect(response).to have_http_status(200)
-      expect(body.size).to eq(100)
+      expect(body).to have_key('repository_name')
+      expect(body['repository_name']).to eq('technical-challenge')
+    end
+
+    it 'search for a non existent repository' do
+      get '/api/v1/repos/search', params: { repository_name: "technical_challenge" }
+      body = JSON.parse(response.body)
+
+      expect(body['message']).to eq('Repository Not Found')
     end
   end
 end
